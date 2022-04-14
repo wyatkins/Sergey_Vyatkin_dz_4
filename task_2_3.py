62bf6668e05a211a6feaec90e1ecafeac7d8da94
sergei <wyatkins@yandex.ru>
2022-04-14 18:50:47 +0500

    add file

---
+++ task_2_3.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+
+# import decimal
+from datetime import datetime
+
+import requests
+
+
+def currency_rates(currency_code="", url="http://www.cbr.ru/scripts/XML_daily.asp"):
+    """ we get the current rate by currency code from the site """
+
+    if not (currency_code and url):
+        return None
+
+    # fix register
+    currency_code = currency_code.upper()
+
+    # take respond from url
+    respond = requests.get(url)
+
+    if respond.ok:
+
+        cur = respond.text.split(currency_code)
+
+        # if no found currency
+        if len(cur) == 1:
+            return None
+
+        # take value param from text
+        value = cur[1].split("</Value>")[0].split("<Value>")[1]
+
+        # conver to float
+        value = float(value.replace(",", "."))
+
+        # conver to decimal
+        #decimal.getcontext().prec = 4
+        #value = decimal.Decimal(value.replace(",", "."))
+
+        # parse date from respond
+        date = respond.headers["Date"]
+        date = datetime.strptime(date, "%a, %d %b %Y %H:%M:%S GMT").date()
+
+        return (value, date)
+
+    else:
+        return None
+
+
+print((currency_rates("Usd")))
+print((currency_rates("eUR")))
